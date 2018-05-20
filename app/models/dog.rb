82028f88ac3d7bf1129b441c8358b1227a793f26
Yina Zhu <yinagit@gmail.com>
2018-05-20 16:19:32 +0000

    automatically backed up by learn

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -4,11 +4,11 @@ class Dog
 
   @all = []
 
-  # def initialize(name, breed, age)
-  #   @name = name
-  #   @breed = breed
-  #   @age = age
-  # end
+  def initialize(name, breed, age)
+    @name = name
+    @breed = breed
+    @age = age
+  end
 
   def save
     @all << Dog.new
